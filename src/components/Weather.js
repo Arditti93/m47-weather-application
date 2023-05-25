7abce66d59b31991b3222d48490ca7790fa91aa0
Cottom1993 <jamiecottom18@gmail.com>
2023-05-25 12:06:16 +0100

    Weather Form implemented

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,7 +6,10 @@ const Weather = () => {
     const [city,setCity] = useState();   
     return (
         <div className="weatherBox">
-            
+            <form>
+                <input onChange={(event) => setCity(event.target.value)} ></input>
+                <button type="submit">Check Weather</button>
+            </form>
                 
         </div>
     )
