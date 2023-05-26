72e4518e6de21b2a5f4576b56ec0adee189d0e44
mjfarrelm <matthewfarrelm@googlemail.com>
2023-05-26 15:46:14 +0100

    css files created (wip)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+
 import React from "react";
 import {useState} from 'react';
 import Weather from "./components/Weather";
-
+import './App.css';
+// import SunnySky from "./src/Images/SunnySky.jpeg"
 const App = () => {  
   
   return (
-  <div> 
+    
+  <div className="display"> 
+  {/* <img src={SunnySky} /> */}
     <div>
       <h1>M47 Weather App</h1>
+      <br></br>
       <p>Enter a City to search for the Weather</p>
     </div>
-    
+    <br></br>
     <div className="App">
       <Weather/>
     </div>
