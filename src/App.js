79262ba9a7ec49177e9a3f0ac75acfe9923e9dda
Ashleighalex <ashleighalexandra199824@gmail.com>
2023-05-26 18:36:20 +0100

    Styled the web page including background (ASH)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-
 import React from "react";
-import {useState} from 'react';
 import Weather from "./components/Weather";
 import './App.css';
-// import SunnySky from "./src/Images/SunnySky.jpeg"
+import SunnySky from "./Images/weather.jpg";
+
 const App = () => {  
-  
   return (
-    
-  <div className="display"> 
-  {/* <img src={SunnySky} /> */}
-    <div>
-      <h1>M47 Weather App</h1>
-      <br></br>
-      <p>Enter a City to search for the Weather</p>
-    </div>
-    <br></br>
-    <div className="App">
-      <Weather/>
-    </div>
-  </div> 
+    <div className="display"> 
+      <div>
+        <h1>M47 Weather App</h1>
+        <br />
+        <p>Enter a City to search for the Weather</p>
+      </div>
+      <br />
+      <div className="App">
+        <Weather />
+      </div>
+      <div>
+        <img src={SunnySky} alt="Myimage" />
+      </div>
+    </div> 
   );
 };
 
 export default App;
+
