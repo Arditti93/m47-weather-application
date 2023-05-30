7f2275ce308b7ddbf27ddf0cce4e926c56cb6162
Cottom1993 <jamiecottom18@gmail.com>
2023-05-30 13:37:52 +0100

    Further CSS organising layout

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -11,6 +11,9 @@ function DisplayWeather({data}) {
       <h2>Weather Details for {name}, {sys.country}</h2>
       <p>Current Weather: {weather[0].main}</p>
       <p>Temperature: {main.temp}</p>
+      <p>Feels Like: {main.feels_like}</p>
+      {/* <p>Wind: {wind}</p> */}
+      <p>Humidity: {main.humidity}</p>
     </div>
   );
 };
